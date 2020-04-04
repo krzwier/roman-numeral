138e2d55acd98c67f0f1e6bd43b78928d5f1d23a
krzwier <krzwier@gmail.com>
2020-04-04 14:08:03 -0500

    First commit.

---
+++ src/main/java/roman_numeral/RomanNumeral.java
@@ -0,0 +1,12 @@
+package roman_numeral;
+
+public final class RomanNumeral {
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/roman_numeral/RomanNumeralTest.java
@@ -0,0 +1,18 @@
+package roman_numeral;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Suite of unit tests for RomanNumeral class.
+ */
+class RomanNumeralTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }
+}
