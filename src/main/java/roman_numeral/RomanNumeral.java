11d0f60a612d388672630916abadeb31a6359c17
krzwier <krzwier@gmail.com>
2020-04-13 22:30:19 -0500

    Refactor and cleanup.

--- src/main/java/roman_numeral/RomanNumeral.java
+++ src/main/java/roman_numeral/RomanNumeral.java
@@ -4,27 +4,38 @@ import java.util.HashMap;
 
 public final class RomanNumeral {
 
-    private final char ONE = 'I';
-    private final char FIVE = 'V';
-    private final char TEN = 'X';
-    private final char FIFTY = 'L';
-    private final char ONE_HUNDRED = 'C';
-    private final char FIVE_HUNDRED = 'D';
-    private final char ONE_THOUSAND = 'M';
-    private final int[] basic_numerals = {1000, 500, 100, 50, 10, 5, 1};
-    private final int[] repeating_numerals = {1000, 100, 10, 1};
+    private final String ONE = "I";
+    private final String FOUR = "IV";
+    private final String FIVE = "V";
+    private final String NINE = "IX";
+    private final String TEN = "X";
+    private final String FORTY = "XL";
+    private final String FIFTY = "L";
+    private final String NINETY = "XC";
+    private final String ONE_HUNDRED = "C";
+    private final String FOUR_HUNDRED = "CD";
+    private final String FIVE_HUNDRED = "D";
+    private final String NINE_HUNDRED = "CM";
+    private final String ONE_THOUSAND = "M";
+    private final int[] basic_numbers = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
 
-    private HashMap<Integer, Character> rn_mappings;
+    private HashMap<Integer, String> rn_mappings;
 
     public RomanNumeral() {
-        // populate hash table with basic numerals
+        // populate hash table with basic numbers
         rn_mappings = new HashMap<>();
         rn_mappings.put(1,ONE);
+        rn_mappings.put(4,FOUR);
         rn_mappings.put(5,FIVE);
+        rn_mappings.put(9,NINE);
         rn_mappings.put(10,TEN);
+        rn_mappings.put(40,FORTY);
         rn_mappings.put(50,FIFTY);
+        rn_mappings.put(90,NINETY);
         rn_mappings.put(100,ONE_HUNDRED);
+        rn_mappings.put(400,FOUR_HUNDRED);
         rn_mappings.put(500,FIVE_HUNDRED);
+        rn_mappings.put(900,NINE_HUNDRED);
         rn_mappings.put(1000,ONE_THOUSAND);
     }
 
@@ -49,137 +60,23 @@ public final class RomanNumeral {
             return "";
         }
 
-        // check to see if i is represented by a basic numeral
+        // check to see if i is represented by a basic number
         if (rn_mappings.containsKey(i)) {
-            return String.valueOf(rn_mappings.get(i));
+            return rn_mappings.get(i);
         }
 
         // RECURSIVE CASES
 
-        if (i - 1000 >= 0) {
-            return "M" + DecimalToRomanRecursive(i - 1000);
-        }
-
-        if (i - 900 >= 0) {
-            return "CM" + DecimalToRomanRecursive(i - 900);
-        }
-
-        if (i - 500 >= 0) {
-            return "D" + DecimalToRomanRecursive(i - 500);
-        }
-
-        if (i - 400 >= 0) {
-            return "CD" + DecimalToRomanRecursive(i - 400);
-        }
-
-        if (i - 100 >= 0) {
-            return "C" + DecimalToRomanRecursive(i - 100);
-        }
-
-        if (i - 90 >= 0) {
-            return "XC" + DecimalToRomanRecursive(i - 90);
-        }
-        
-        if (i - 50 >= 0) {
-            return "L" + DecimalToRomanRecursive(i - 50);
-        }
-
-        if (i - 40 >= 0) {
-            return "XL" + DecimalToRomanRecursive(i - 40);
-        }
-
-        if (i - 10 >= 0) {
-            return "X" + DecimalToRomanRecursive(i - 10);
-        }
-
-        if (i - 9 >= 0) {
-            return "IX" + DecimalToRomanRecursive(i - 9);
-        }
-
-        if (i - 5 >= 0) {
-            return "V" + DecimalToRomanRecursive(i - 5);
-        }
-
-        if (i - 4 >= 0) {
-            return "IV" + DecimalToRomanRecursive(i - 4);
+        for (int n = 0; n < basic_numbers.length - 2; n++ ) {
+            if (i - basic_numbers[n] >= 0) {
+                return rn_mappings.get(basic_numbers[n]) + 
+                    DecimalToRomanRecursive(i - basic_numbers[n]);
+            }
         }
 
         return "I" + DecimalToRomanRecursive(i - 1);
-    }
 
-    
-    /**
-     * This method checks if i is a basic numeral minus another basic numeral
-     * 
-     * @param i
-     * @return roman numeral if there's a match, "NONE" if not
-     */
-    private String BasicMinusBasic(int i) {
-        int trial_number;
         
-        for (int n = basic_numerals.length - 1; n >= 0; n--) {
-            trial_number = basic_numerals[n] - i;
-            if (rn_mappings.containsKey(trial_number)) {
-                return String.valueOf(rn_mappings.get(trial_number)) + 
-                    String.valueOf(rn_mappings.get(basic_numerals[n]));
-            }
-        }
-
-        return "NONE";
-    }
-
-    /**
-     * This method checks if i is a basic numeral plus another basic numeral
-     * 
-     * @param i
-     * @return roman numeral if there's a match, "NONE" if not
-     */
-    private String BasicPlusBasic(int i) {
-        int trial_number;
-        
-        for (int n = 0; n < basic_numerals.length; n++ ){
-            trial_number = i + basic_numerals[n];
-            if (rn_mappings.containsKey(trial_number)) {
-                return String.valueOf(rn_mappings.get(basic_numerals[n])) +
-                    String.valueOf(rn_mappings.get(trial_number));
-            }
-        }
-
-        return "NONE";
-    }
-
-    /**
-     * This method checks if i needs a repeating numeral
-     * 
-     * @param i
-     * @return roman numeral if there's a match, "NONE" if not
-     */
-    private String RepeatingNumeral(int i) {
-        int trial_number;
-        
-        for (int n = repeating_numerals.length - 1; n >= 0; n--) {
-            // check for repeating numeral times 2
-            trial_number = i - (repeating_numerals[n] * 2);
-            if (rn_mappings.containsKey(trial_number) && 
-                    (repeating_numerals[n] <= trial_number)) {
-                return  String.valueOf(rn_mappings.get(trial_number)) +
-                    String.valueOf(rn_mappings.get(repeating_numerals[n])) +
-                    String.valueOf(rn_mappings.get(repeating_numerals[n]));
-            }
-
-            // check for repeating numeral times 3
-            trial_number = i - (repeating_numerals[n] * 3);
-            if (rn_mappings.containsKey(trial_number) &&
-                    (repeating_numerals[n] <= trial_number)) {
-                return String.valueOf(rn_mappings.get(trial_number)) +
-                    String.valueOf(rn_mappings.get(repeating_numerals[n])) +
-                    String.valueOf(rn_mappings.get(repeating_numerals[n])) +
-                    String.valueOf(rn_mappings.get(repeating_numerals[n]));
-                    
-            }
-        }
-
-        return "NONE";
     }
 
 }
